265a7589f35e340c585380728834fcbaca929a73
Hong You Wong <hwong@u80z05s03.42kl.edu.my>
2023-03-25 18:00:26 +0800

    ex00 tweaked main

--- ex00/main.cpp
+++ ex00/main.cpp
@@ -1,11 +1,13 @@
 #include "Bureaucrat.hpp"
 
 int main( void ) {
+	std::cout << "[ TESTING CONSTRUCTS ]" << std::endl;
 	Bureaucrat();
 	Bureaucrat("Bob");
 	Bureaucrat(150);
 	Bureaucrat("Cat", 130);
 
+	std::cout << "[ TESTING WRONG VALUE CONSTRUCTS ]" << std::endl;
 	Bureaucrat(160);
 	Bureaucrat("Poggies", 0);
 }
